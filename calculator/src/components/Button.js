e8c54d4626b5b9b2dc628a097fd14bb522623fa2
Caio Telles <cast@icomp.ufam.edu.br>
2020-07-13 13:55:42 -0400

    Clicks

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -3,9 +3,21 @@ import {Text, Dimensions, TouchableHighlight} from 'react-native';
 import Styles from '../styles/Styles';
 
 export default props => {
+  const stylesButton = [Styles.button];
+
+  if (props.double) {
+    stylesButton.push(Styles.buttonDouble);
+  }
+  if (props.triple) {
+    stylesButton.push(Styles.buttonTriple);
+  }
+  if (props.operation) {
+    stylesButton.push(Styles.operationButton);
+  }
+
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={Styles.button}> {props.label} </Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}> {props.label} </Text>
     </TouchableHighlight>
   );
 };
--- calculator/src/styles/Styles.js
+++ calculator/src/styles/Styles.js
@@ -11,6 +11,16 @@ export default StyleSheet.create({
     borderWidth: 1,
     borderColor: '#888',
   },
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
